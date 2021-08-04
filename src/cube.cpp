9217b6f716fab5ebc073e42d79a2b3fdde895111
443999592@qq.com <443999592@qq.com>
2021-08-04 21:37:40 +0800

    remove old code

--- src/cube.cpp
+++
@@ -1,7 +0,0 @@
-#include "cube.h"
-
-namespace CubeObject {
-	uint32_t get_size() {
-        return sizeof(cube_vertices[0]) * cube_vertices.size();
-    }
-}
--- src/user_interface.cpp
+++
@@ -1,4 +0,0 @@
-#include "user_interface.h"
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-
